65e8d073802b7c0eb65c9b2165024081551f5590
Gökçen Akgül <99014934+gkcena@users.noreply.github.com>
2026-01-09 09:17:42 +0300

    staff audio

--- Assets/_EdgeOfDark/Script/Weapons/Staff.cs
+++ Assets/_EdgeOfDark/Script/Weapons/Staff.cs
@@ -17,6 +17,11 @@ public class Staff : MonoBehaviour
     [SerializeField] private float damageAmount = 0.2f;
     [SerializeField] private bool destroyOnHit = true;
 
+    [Header("Sound Effects")]
+    public AudioClip enemyHitSound;
+    public AudioClip enemyDeathSound;
+    public AudioClip castSound;
+
     [Header("Rate Limit")]
     [SerializeField] private float cooldownSeconds = 0.5f;
 
@@ -59,6 +64,8 @@ public class Staff : MonoBehaviour
             return;
         }
 
+        PlayCastSound();
+
         Vector3 spawnPos = skillSpawnPoint.position;
         Quaternion spawnRot = Quaternion.LookRotation(cam.transform.forward, Vector3.up);
 
@@ -67,7 +74,7 @@ public class Staff : MonoBehaviour
         var proj = skill.GetComponent<StaffProjectile>();
         if (proj == null) proj = skill.AddComponent<StaffProjectile>();
 
-        proj.Init(damageAmount, destroyOnHit);
+        proj.Init(damageAmount, destroyOnHit, enemyHitSound, enemyDeathSound);
 
         EnsurePhysics(skill);
 
@@ -82,6 +89,15 @@ public class Staff : MonoBehaviour
             {
                 if (skill != null) Destroy(skill);
             });
+    }
+
+    private void PlayCastSound()
+    {
+        if (castSound != null && SFXManager.Instance != null)
+        {
+            SFXManager.Instance.PlaySFX(castSound);
+            Debug.Log("Staff cast sound played!");
+        }
     }
 
     private void EnsurePhysics(GameObject skill)
@@ -111,13 +127,16 @@ public class Staff : MonoBehaviour
     {
         private float damageAmount;
         private bool destroyOnHit;
-
+        private AudioClip enemyHitSound;
+        private AudioClip enemyDeathSound;
         private bool hasHit;
 
-        public void Init(float damage, bool destroyOnHit)
+        public void Init(float damage, bool destroyOnHit, AudioClip hitSound, AudioClip deathSound)
         {
             this.damageAmount = damage;
             this.destroyOnHit = destroyOnHit;
+            this.enemyHitSound = hitSound;
+            this.enemyDeathSound = deathSound;
         }
 
         private void OnTriggerEnter(Collider other)
@@ -137,13 +156,68 @@ public class Staff : MonoBehaviour
 
             if (filledBar.fillAmount <= 0f)
             {
+                TriggerEnemyDeathAnimation(other.gameObject);
+                PlayEnemyDeathSound();
                 other.gameObject.SetActive(false);
+            }
+            else
+            {
+                TriggerEnemyHitAnimation(other.gameObject);
+                PlayEnemyHitSound();
             }
 
             if (destroyOnHit)
             {
                 transform.DOKill();
                 Destroy(gameObject);
+            }
+        }
+
+        private void TriggerEnemyHitAnimation(GameObject enemy)
+        {
+            Animator enemyAnimator = enemy.GetComponent<Animator>();
+            if (enemyAnimator == null)
+            {
+                enemyAnimator = enemy.GetComponentInChildren<Animator>();
+            }
+
+            if (enemyAnimator != null)
+            {
+                enemyAnimator.SetTrigger("GetHit");
+                Debug.Log($"Enemy {enemy.name} hit animation triggered by staff!");
+            }
+        }
+
+        private void TriggerEnemyDeathAnimation(GameObject enemy)
+        {
+            Animator enemyAnimator = enemy.GetComponent<Animator>();
+            if (enemyAnimator == null)
+            {
+                enemyAnimator = enemy.GetComponentInChildren<Animator>();
+            }
+
+            if (enemyAnimator != null)
+            {
+                enemyAnimator.SetTrigger("Death");
+                Debug.Log($"Enemy {enemy.name} death animation triggered by staff!");
+            }
+        }
+
+        private void PlayEnemyHitSound()
+        {
+            if (enemyHitSound != null && SFXManager.Instance != null)
+            {
+                SFXManager.Instance.PlaySFX(enemyHitSound);
+                Debug.Log("Staff enemy hit sound played!");
+            }
+        }
+
+        private void PlayEnemyDeathSound()
+        {
+            if (enemyDeathSound != null && SFXManager.Instance != null)
+            {
+                SFXManager.Instance.PlaySFX(enemyDeathSound);
+                Debug.Log("Staff enemy death sound played!");
             }
         }
 
